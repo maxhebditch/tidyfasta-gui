da60649ddb5964deb7722e351cb32bd64b1eef7c
Max Hebditch <max@maxhebditch.co.uk>
2020-05-19 15:38:50 +0100

    Split out logic between asking for user inout and continuing if user input says so

--- src/main/java/com/proteinsol/tidyfasta/gui/TidyFASTAGUI.java
+++ src/main/java/com/proteinsol/tidyfasta/gui/TidyFASTAGUI.java
@@ -11,8 +11,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -42,228 +40,30 @@ public class TidyFASTAGUI extends JFrame {
     private JPanel OutputButtonPanel;
     private JPanel OutputTitlePanel;
     private static String ValidFASTA;
+    private StringBuilder ErrorMessage;
 
     public TidyFASTAGUI(String title) {
         super(title);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
 
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                clearOutput();
-                SubmitSequence();
-            }
-
-            public void clearOutput() {
-                numValid.setText("Number of valid sequences: ");
-                numSubmitted.setText("Sequences: ");
-                FASTAOutputArea.setText("");
-            }
-
-            public void SubmitSequence() {
-                String submittedSequences = FASTAInputArea.getText();
-
-                if (submittedSequences.length() == 0) {
-                    JOptionPane.showMessageDialog(mainPanel,
-                            "No sequence submitted");
-                    return;
-                }
-
-                ReadFASTAAndFormat FASTA = new ReadFASTAAndFormat(submittedSequences);
-                int numErrors = FASTA.getNumErrors();
-
-                if (numErrors > 0) {
-                    StringBuilder UserErrorMessage = new StringBuilder();
-
-                    if (FASTA.getValidatedNumber() > 0) {
-                        UserErrorMessage.append(FASTA.getNumErrors());
-                        UserErrorMessage.append("/");
-                        UserErrorMessage.append(FASTA.getSubmittedNumber());
-                        UserErrorMessage.append(" of the sequences submitted contained errors.\n\n");
-                    }
-
-                    if (numErrors == 1) {
-                        UserErrorMessage.append("The following error occurred\n");
-                        UserErrorMessage.append(FASTA.getErrMsg());
-                        UserErrorMessage.append("\n");
-                    } else if (numErrors <= 10) {
-                        UserErrorMessage.append("The following errors occurred\n");
-                        UserErrorMessage.append(FASTA.getErrMsg());
-                        UserErrorMessage.append("\n");
-                    } else {
-                        if (FASTA.getValidatedNumber() == 0) {
-                            UserErrorMessage.append("All ");
-                            UserErrorMessage.append(FASTA.getNumErrors());
-                            UserErrorMessage.append(" sequences contained errors, the first 10 errors were:\n\n");
-                        } else {
-                            UserErrorMessage.append("The first 10 errors were:\n\n");
-                        }
-
-                        String[] ErrorArray = FASTA.getErrMsgArray();
-                        for (int i = 0; i < 10; i++) {
-                            UserErrorMessage.append(ErrorArray[i]);
-                            UserErrorMessage.append("\n");
-                        }
-                        UserErrorMessage.append("\n");
-                    }
-
-                    if (FASTA.getValidatedNumber() > 0) {
-                        UserErrorMessage.append("\nWould you like to continue?");
-                        int dialogResult = JOptionPane.showConfirmDialog(mainPanel,
-                                UserErrorMessage.toString(),
-                                "FASTA Errors found",
-                                JOptionPane.YES_NO_OPTION);
-                        if (dialogResult == JOptionPane.NO_OPTION) {
-                            return;
-                        }
-                    } else {
-                        JOptionPane.showMessageDialog(mainPanel, UserErrorMessage.toString());
-                        return;
-                    }
-                }
-
-                numValid.setText("Number of valid sequences: " + FASTA.getValidatedNumber());
-                numSubmitted.setText("Sequences: " + FASTA.getSubmittedNumber());
-
-                StringBuilder FASTAOutputText = new StringBuilder();
-
-                for (FASTAObject validFasta : FASTA.getArrayFASTA()) {
-                    FASTAOutputText.append(validFasta.ID);
-                    FASTAOutputText.append("\n");
-                    FASTAOutputText.append(validFasta.Sequence);
-                    FASTAOutputText.append("\n\n");
-                }
-
-                if (FASTAOutputText.length() > 2) {
-                    FASTAOutputText.setLength(FASTAOutputText.length() - 2);
-                } else {
-                    FASTAOutputText.setLength(0);
-                }
-
-                ValidFASTA = FASTAOutputText.toString();
-                FASTAOutputArea.setText(ValidFASTA);
-            }
+        submitButton.addActionListener(e -> {
+            clearOutput();
+            SubmitSequence();
         });
 
-        copyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                CopyValidFASTAToClipboard();
-            }
+        copyButton.addActionListener(e -> CopyValidFASTAToClipboard());
 
-            public void CopyValidFASTAToClipboard() {
-                StringSelection ValidFASTASelect = new StringSelection(ValidFASTA);
-                Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
-                cb.setContents(ValidFASTASelect, null);
-            }
-        });
+        openButton.addActionListener(e -> openFile());
 
-        openButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                openFile();
-            }
-
-            public void openFile() {
-
-                final JFileChooser chooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                        "FASTA/TEXT FILES", "txt", "text", "fasta", "fa", "FASTA");
-                chooser.setFileFilter(filter);
-                int chooserResult = chooser.showOpenDialog(mainPanel);
-
-                File FASTAFile;
-                if (chooserResult == JFileChooser.APPROVE_OPTION) {
-                    FASTAFile = chooser.getSelectedFile();
-                    FASTAInputArea.setText("");
-                    FASTAOutputArea.setText("");
-                } else {
-                    return;
-                }
-
-                List<String> lines = Collections.emptyList();
-                Path FASTAFileName = FASTAFile.toPath();
-
-                //refactor
-                try {
-                    lines = Files.readAllLines(FASTAFileName, StandardCharsets.UTF_8);
-                } catch (IOException ioException) {
-                    JOptionPane.showMessageDialog(mainPanel,
-                            "File " + FASTAFileName.toString() + " not found");
-                }
-
-                StringBuilder result = new StringBuilder();
-
-                for (String line : lines) {
-                    result.append(line);
-                    result.append("\n");
-                }
-                result.setLength(result.length() - 1);
-
-                FASTAInputArea.setText(result.toString());
-            }
-        });
-
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (ValidFASTA != null) {
-                    writeFile();
-                } else {
-                    JOptionPane.showMessageDialog(mainPanel,
-                            "Submit file before saving.");
-                }
-            }
-
-            public void writeFile() {
-                JFileChooser saveChooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                        "FASTA/TEXT FILES", "txt", "text", "fasta", "fa", "FASTA");
-                saveChooser.setFileFilter(filter);
-                saveChooser.setDialogTitle("Specify a location to save");
-                saveChooser.setSelectedFile(new File("tidyfasta.fasta"));
-
-                int userSelection = saveChooser.showSaveDialog(mainPanel);
-
-                File fileToSave;
-                if (userSelection == JFileChooser.APPROVE_OPTION) {
-                    fileToSave = saveChooser.getSelectedFile();
-                } else {
-                    return;
-                }
-
-                String fileString = fileToSave.toString();
-
-                if (!fileString.contains(".")) {
-                    fileString = fileString + ".fasta";
-                }
-
-                Path SaveFileName = Paths.get(fileString);
-
-                if (Files.exists(SaveFileName)) {
-
-                    int dialogResult = JOptionPane.showConfirmDialog(mainPanel,
-                            "File " + SaveFileName.toString() + " already exists." +
-                                    "\nWould you like to overwrite it?",
-                            "FASTA Errors found",
-                            JOptionPane.YES_NO_OPTION);
-                    if (dialogResult == JOptionPane.NO_OPTION) {
-                        return;
-                    }
-                }
-
-                try {
-                    Files.write(SaveFileName, Collections.singleton(ValidFASTA));
-                    JOptionPane.showMessageDialog(mainPanel,
-                            "File " + SaveFileName.toString() + " saved!");
-                } catch (IOException err) {
-                    JOptionPane.showMessageDialog(mainPanel,
-                            "File " + SaveFileName.toString() + " not writable.");
-                }
+        saveButton.addActionListener(e -> {
+            if (ValidFASTA != null) {
+                writeFile();
+            } else {
+                JOptionPane.showMessageDialog(mainPanel,
+                        "Submit file before saving.");
             }
         });
     }
@@ -271,6 +71,207 @@ public class TidyFASTAGUI extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new TidyFASTAGUI("Tidy FASTA");
         frame.setVisible(true);
+    }
+
+    public void clearOutput() {
+        numValid.setText("Number of valid sequences: ");
+        numSubmitted.setText("Sequences: ");
+        FASTAOutputArea.setText("");
+    }
+
+    public void SubmitSequence() {
+        String submittedSequences = FASTAInputArea.getText();
+
+        if (submittedSequences.length() == 0) {
+            JOptionPane.showMessageDialog(mainPanel,
+                    "No sequence submitted");
+            return;
+        }
+
+        ReadFASTAAndFormat FASTA = new ReadFASTAAndFormat(submittedSequences);
+
+        if (ContinueWithAnalysis(FASTA)) {
+            numValid.setText("Number of valid sequences: " + FASTA.getValidatedNumber());
+            numSubmitted.setText("Sequences: " + FASTA.getSubmittedNumber());
+
+            StringBuilder FASTAOutputText = new StringBuilder();
+
+            for (FASTAObject validFasta : FASTA.getArrayFASTA()) {
+                FASTAOutputText.append(validFasta.ID);
+                FASTAOutputText.append("\n");
+                FASTAOutputText.append(validFasta.Sequence);
+                FASTAOutputText.append("\n\n");
+            }
+
+            if (FASTAOutputText.length() > 2) {
+                FASTAOutputText.setLength(FASTAOutputText.length() - 2);
+            } else {
+                FASTAOutputText.setLength(0);
+            }
+
+            ValidFASTA = FASTAOutputText.toString();
+            FASTAOutputArea.setText(ValidFASTA);
+        }
+        ;
+    }
+
+    public boolean ContinueWithAnalysis(ReadFASTAAndFormat FASTA) {
+
+        if (FASTA.getNumErrors() > 0) {
+            BuildErrorMessage(FASTA);
+            return TestUserDesire(FASTA);
+        } else {
+            return true;
+        }
+
+    }
+
+    public void CopyValidFASTAToClipboard() {
+        StringSelection ValidFASTASelect = new StringSelection(ValidFASTA);
+        Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+        cb.setContents(ValidFASTASelect, null);
+    }
+
+    public void openFile() {
+
+        final JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "FASTA/TEXT FILES", "txt", "text", "fasta", "fa", "FASTA");
+        chooser.setFileFilter(filter);
+        int chooserResult = chooser.showOpenDialog(mainPanel);
+
+        File FASTAFile;
+        if (chooserResult == JFileChooser.APPROVE_OPTION) {
+            FASTAFile = chooser.getSelectedFile();
+            FASTAInputArea.setText("");
+            FASTAOutputArea.setText("");
+        } else {
+            return;
+        }
+
+        List<String> lines = Collections.emptyList();
+        Path FASTAFileName = FASTAFile.toPath();
+
+        //refactor
+        try {
+            lines = Files.readAllLines(FASTAFileName, StandardCharsets.UTF_8);
+        } catch (IOException ioException) {
+            JOptionPane.showMessageDialog(mainPanel,
+                    FASTAFileName.toString() + " not found");
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (String line : lines) {
+            result.append(line);
+            result.append("\n");
+        }
+        result.setLength(result.length() - 1);
+
+        FASTAInputArea.setText(result.toString());
+    }
+
+    public void writeFile() {
+        JFileChooser saveChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "FASTA/TEXT FILES", "txt", "text", "fasta", "fa", "FASTA");
+        saveChooser.setFileFilter(filter);
+        saveChooser.setDialogTitle("Specify a location to save");
+        saveChooser.setSelectedFile(new File("tidyfasta.fasta"));
+
+        int userSelection = saveChooser.showSaveDialog(mainPanel);
+
+        File fileToSave;
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            fileToSave = saveChooser.getSelectedFile();
+        } else {
+            return;
+        }
+
+        String fileString = fileToSave.toString();
+
+        if (!fileString.contains(".")) {
+            fileString = fileString + ".fasta";
+        }
+
+        Path saveFileName = Paths.get(fileString);
+
+        if (Files.exists(saveFileName)) {
+
+            int dialogResult = JOptionPane.showConfirmDialog(mainPanel,
+                    saveFileName.toString() + " already exists." +
+                            "\nWould you like to overwrite it?",
+                    "FASTA Errors found",
+                    JOptionPane.YES_NO_OPTION);
+            if (dialogResult == JOptionPane.NO_OPTION) {
+                return;
+            }
+        }
+
+        try {
+            Files.write(saveFileName, Collections.singleton(ValidFASTA));
+            JOptionPane.showMessageDialog(mainPanel,
+                    saveFileName.toString() + " saved!");
+        } catch (IOException err) {
+            JOptionPane.showMessageDialog(mainPanel,
+                    saveFileName.toString() + " not writable.");
+        }
+    }
+
+    public void BuildErrorMessage(ReadFASTAAndFormat FASTA) {
+
+        StringBuilder userErrorMessage = new StringBuilder();
+
+        if (FASTA.getValidatedNumber() > 0) {
+            userErrorMessage.append(FASTA.getNumErrors());
+            userErrorMessage.append("/");
+            userErrorMessage.append(FASTA.getSubmittedNumber());
+            userErrorMessage.append(" of the sequences submitted contained errors.\n\n");
+        }
+
+        if (FASTA.getNumErrors() == 1) {
+            userErrorMessage.append("The following error occurred\n");
+            userErrorMessage.append(FASTA.getErrMsg());
+            userErrorMessage.append("\n");
+        } else if (FASTA.getNumErrors() <= 10) {
+            userErrorMessage.append("The following errors occurred\n");
+            userErrorMessage.append(FASTA.getErrMsg());
+            userErrorMessage.append("\n");
+        } else {
+            if (FASTA.getValidatedNumber() == 0) {
+                userErrorMessage.append("All ");
+                userErrorMessage.append(FASTA.getNumErrors());
+                userErrorMessage.append(" sequences contained errors, the first 10 errors were:\n\n");
+            } else {
+                userErrorMessage.append("The first 10 errors were:\n\n");
+            }
+
+            String[] errorArray = FASTA.getErrMsgArray();
+            for (int i = 0; i < 10; i++) {
+                userErrorMessage.append(errorArray[i]).append("\n");
+            }
+            userErrorMessage.append("\n");
+        }
+
+        this.ErrorMessage = userErrorMessage;
+    }
+
+    public boolean TestUserDesire(ReadFASTAAndFormat FASTA) {
+        if (FASTA.getValidatedNumber() > 0) {
+
+            String ErrorDialog = this.ErrorMessage.toString() + "\nWould you like to continue?";
+
+            int dialogResult = JOptionPane.showConfirmDialog(mainPanel,
+                    ErrorDialog,
+                    "FASTA Errors found",
+                    JOptionPane.YES_NO_OPTION);
+
+            return dialogResult != JOptionPane.NO_OPTION;
+
+        } else {
+            JOptionPane.showMessageDialog(mainPanel, this.ErrorMessage.toString());
+            return false;
+        }
     }
 
     {
